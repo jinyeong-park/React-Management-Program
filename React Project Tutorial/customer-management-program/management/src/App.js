a84d847dcdd674144056ddc9eaebacfaa8ffa0f7
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-03-10 00:22:19 -0700

    changes gray background

--- React Project Tutorial/customer-management-program/management/src/App.js
+++ React Project Tutorial/customer-management-program/management/src/App.js
@@ -5,11 +5,9 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello React Project!</p>
-        </header>
+      <div className="gray-background">
+        <img src={logo} lat="logo" />
+        <h2>Let's develop management system!</h2>
       </div>
     );
   }
